46610cb588b333b9d0247ed753ff75d1692ad95e
Atul Varma <varmaa@gmail.com>
2015-12-04 11:29:45 -0500

    Added trivial example.

---
+++ example/build.py
@@ -0,0 +1,16 @@
+import os
+
+from govlabstatic.cli import GovLabManager
+
+manager = GovLabManager(
+    site_name='example site',
+    site={
+        'outpath': 'site',
+
+    },
+    sass_src_path=os.path.join('sass', 'styles.scss'),
+    sass_dest_path=os.path.join('site', 'static', 'styles', 'styles.css')
+)
+
+if __name__ == '__main__':
+    manager.run()
