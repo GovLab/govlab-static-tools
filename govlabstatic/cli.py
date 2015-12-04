69a35ed3b135c5120ed51e5de5b89b2e2c12ce1d
Atul Varma <varmaa@gmail.com>
2015-12-04 10:33:42 -0500

    cli now uses govlabstatic.script.

--- govlabstatic/cli.py
+++ govlabstatic/cli.py
@@ -7,6 +7,8 @@ import colorama
 import staticjinja
 
 from . import sass, webserver
+from .script.manager import Manager, App
+from .script.commands import Command
 
 def init_colors():
     if sys.platform == 'win32':
@@ -15,15 +17,39 @@ def init_colors():
             del os.environ['TERM']
     colorama.init()
 
+class GovLabApp(App):
+    def __init__(self, **kwargs):
+        App.__init__(self)
+        self.__dict__.update(kwargs)
+
+class GovLabManager(Manager):
+    def __init__(self, site_name, usage=None, help=None, **kwargs):
+        app = GovLabApp(site_name=site_name, **kwargs)
+        Manager.__init__(
+            self,
+            app,
+            usage=usage,
+            help=help,
+            description='Static site generator for %s' % site_name
+        )
+        self.add_command('runserver', WatchBuildServe())
+
+    def run(self, commands=None, default_command=None):
+        init_colors()
+        Manager.run(self, commands, default_command)
+
+class WatchBuildServe(Command):
+    def run(self):
+        sass.start_watch(src_path=self.app.sass_src_path,
+                         dest_path=self.app.sass_dest_path)
+        webserver.start(root_dir=self.app.site['outpath'])
+        staticjinja.make_site(**self.app.site).render(use_reloader=True)
+
 def run(sass_src_path, sass_dest_path, site, name):
-    init_colors()
-
-    parser = argparse.ArgumentParser(
-        description='Static site generator for %s' % name,
+    manager = GovLabManager(
+        site_name=name,
+        sass_src_path=sass_src_path,
+        sass_dest_path=sass_dest_path,
+        site=site
     )
-
-    args = parser.parse_args()
-
-    sass.start_watch(src_path=sass_src_path, dest_path=sass_dest_path)
-    webserver.start(root_dir=site['outpath'])
-    staticjinja.make_site(**site).render(use_reloader=True)
+    manager.run(default_command='runserver')
