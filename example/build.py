65b97e1e453a33894e9f9337518731d28275d25f
Atul Varma <varmaa@gmail.com>
2015-12-07 10:53:30 -0500

    update example to use new API

--- example/build.py
+++ example/build.py
@@ -1,13 +1,11 @@
 import os
 
+import staticjinja
 from govlabstatic.cli import Manager
 
 manager = Manager(
     site_name='example site',
-    site={
-        'outpath': 'site',
-
-    },
+    site=staticjinja.make_site(outpath='site'),
     sass_src_path=os.path.join('sass', 'styles.scss'),
     sass_dest_path=os.path.join('site', 'static', 'styles', 'styles.css')
 )
