4038756f42fdfaf1282a535686ce89f308c8577a
Atul Varma <varmaa@gmail.com>
2015-12-07 22:11:36 -0500

    hopefully fix rtfd.org docs.

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,9 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+sys.path.insert(0, os.path.abspath('..'))
+import govlabstatic
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
