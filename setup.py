7092e0db245620275979310120ce8ec1725aafaf
Atul Varma <varmaa@gmail.com>
2015-12-09 12:23:43 -0500

    fix url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='govlab-static-tools',
       version='0.1',
       description='Tools for GovLab static site generation',
-      url='http://github.com/govlab/govlab-static-site-tools',
+      url='http://github.com/govlab/govlab-static-tools',
       author='Atul Varma',
       author_email='varmaa@gmail.com',
       license='MIT',
